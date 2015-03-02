c87e96e370cd3744ee78483a5ea6b2d8fd3db105
scottteran <wayteran@gmail.com>
2015-03-02 13:18:56 -0700

    updated files, but there is still an issue with keydown() and mouseenter()

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+	gameTime();
+});
+
+
+function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+}
+
+function gameTime() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -9,9 +20,10 @@ $(document).ready(function() {
 	})
 	.mousedown(function() {
 		playHadouken();
-		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show().animate(
+		$('.hadouken').finish().show()
+		.animate(
 			{'left': '1020px'},
 			500,
 			function() {
@@ -24,13 +36,27 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
-});
 
-function playHadouken () {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
-}	
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
+}
+
+
+
+
+
+
+
 
 
 
